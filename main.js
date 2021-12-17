51d31f6b5052f04b61c91757ff5b253a4ce94739
Piron Evgeniy <piron.johny@gmail.com>
2021-12-17 09:43:09 +0000

    Доделал

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const item = document.querySelector(".item");
 const placeholders = document.querySelectorAll(".placeholder");
 
+let itemHeightValue = 0;
+
 item.addEventListener("dragstart", dragStart);
 item.addEventListener("dragend", dragEnd);
 
@@ -12,6 +14,7 @@ placeholders.forEach((placeholder) => {
 });
 
 function dragStart(e) {
+  itemHeightValue = e.target.clientHeight
   e.target.classList.add("hold");
   setTimeout(() => e.target.classList.add("hide"), 0);
 }
@@ -27,11 +30,14 @@ function dragOver(e) {
 
 function dragEnter(e) {
   e.target.classList.add("hovered");
+  e.target.style.height = itemHeightValue + 'px';
 }
 function dragLeave(e) {
   e.target.classList.remove("hovered");
+  e.target.style.height = null;
 }
 function dragDrop(e) {
   e.target.append(item);
   e.target.classList.remove("hovered");
 }
+
